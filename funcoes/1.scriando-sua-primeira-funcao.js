994f6337f6cdf2d70f2ccd3bf09ec4e406bc33dc
JrCGO <juniosgoulart@gmail.com>
2024-01-25 18:59:53 -0300

    primeiro-verificador

--- funcoes/1.scriando-sua-primeira-funcao.js
+++ funcoes/1.scriando-sua-primeira-funcao.js
@@ -1,11 +1,40 @@
-torrar()
-intejarPao()
+inserirPao()
+selecionarTemp()
+ligarTorradeira()
+torrarPao()
+desligarTorradeira
+injetarPao()
 
-function torrar(){
-    console.log("Torrando pão")
+function inserirPao(){
+    console.log("Inserindo Pão")
+    console.log("Pão reconhecido com sucesso\n")
+    console.log("Aguardando selecionar temperatura")
 }
 
-function intejarPao(){
-    console.log("Preparando para injetar o pão")
-    console.log("Pão ejetado com sucesso")
+function selecionarTemp(){
+    console.log("Temperatura 5 selecioanda\n")
+}
+
+function ligarTorradeira(){
+    console.log("Botão ligar apertado")
+    console.log("Pão reconhecido")
+    console.log("Seleção de temperatura reconhecida")
+    console.log("Permitido para ligar")
+    console.log("Torradeira ligada\n")
+}
+
+function torrarPao(){
+    console.log("Aquecendo torradeira")
+    console.log("Temperatura 5 atingida")
+    console.log("Preparar para desligar torradeira")
+    console.log("Preparar para ejetar Pão\n")
+}
+
+function desligarTorradeira(){
+    console.log("Torradeira desligada\n")   
+}
+
+function injetarPao(){
+    console.log("Injetando Pão")
+    console.log("Pão injetado")
 }
