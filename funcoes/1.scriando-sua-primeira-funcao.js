77e542f274d2dfca61a4ef3a4c17928131f9ed57
JrCGO <juniosgoulart@gmail.com>
2024-01-25 18:58:35 -0300

    primeira-funcao

---
+++ funcoes/1.scriando-sua-primeira-funcao.js
@@ -0,0 +1,11 @@
+torrar()
+intejarPao()
+
+function torrar(){
+    console.log("Torrando pão")
+}
+
+function intejarPao(){
+    console.log("Preparando para injetar o pão")
+    console.log("Pão ejetado com sucesso")
+}
